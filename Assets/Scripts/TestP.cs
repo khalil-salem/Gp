3e8b44436254390c23587c5610c4c929241f2921
khalil-salem <kisalem17@cit.just.edu.jo>
2021-01-12 01:15:06 +0200

    GP files

---
+++ Assets/Scripts/TestP.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestP : MonoBehaviour
+{
+    Text itemNamePanel;
+    public Item item;
+    public GameObject Panel;
+    [SerializeField] private string selectTag = "Select";
+    void Start() {
+        itemNamePanel = Panel.GetComponent<Text>();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform;
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                
+                if (selectionRenderer!=null)
+               {
+                    
+                   // if (selection.CompareTag(selectTag)) {
+                        if (Panel != null)
+                        {
+                            bool isActive = Panel.activeSelf;
+                        Debug.Log(isActive);
+                            Panel.SetActive(!isActive);
+                           Debug.Log(item.price);
+                        }
+                    //}
+                    
+
+                }
+
+
+
+            }
+
+        }
+    }
+    //void UnityApiMouseEvents()
+    //{
+    //    RaycastHit hit;
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        if (hit.rigidbody != null)
+    //            hit.rigidbody.gameObject.SendMessage("OnMouseDown");
+    //        else
+    //            hit.collider.SendMessage("OnMouseDown");
+    //    }
+
+    //}
+}
---
+++ Assets/Scripts/menuPopUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuPopUp : MonoBehaviour
+{
+    
+    public GameObject Panel;
+    [SerializeField] private string selectableTag = "Selectable";
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform;
+                
+                if (selection.CompareTag(selectableTag)) {
+                    //var selectionRenderer = selection.GetComponent<Renderer>();
+                    //if (selectionRenderer)
+                    //{
+                        if (Panel != null)
+                        {
+                            bool isActive = Panel.activeSelf;
+                            Panel.SetActive(!isActive);
+                        }
+                    //}
+                }
+               
+                
+                //if (hit.transform != null)
+                //{
+                //    GameObject item;
+                //    if(item= hit.transform.GetComponent<GameObject>()) { 
+
+                //    if (Panel != null)
+                //    {
+                //        bool isActive = Panel.activeSelf;
+                //        Panel.SetActive(!isActive);
+                //    }
+                //    }
+                //}
+            }
+
+        }
+    }
+}
